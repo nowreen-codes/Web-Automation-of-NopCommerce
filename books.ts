f272d7087627151f2bd952333f663822842102d6
Nowreen Islam <nowreen92519@gmail.com>
2025-01-14 23:26:57 +0600

    Update books.ts

--- books.ts
+++ books.ts
@@ -1,27 +1,51 @@
 import { Page } from "@playwright/test";
+
 export default class Books {
-constructor(public page: Page) {
-}
-async addFirstAndSecondProductsToCart() {
-const addFirstAndSecondProductsToCart = this.page.locator("//button[contains(text(), 'Add to cart')]");
-await addFirstAndSecondProductsToCart.nth(0).click();
-        const popupMessage1 = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
-        await popupMessage1.waitFor({ state: "visible" });
+
+    constructor(public page: Page) {
+    }
+async clearCart() {
+        const cartIcon = this.page.locator("//a[@class='ico-cart']");
+        await cartIcon.hover();
+        await cartIcon.click();
+        const emptyCartMessage = this.page.locator("//span[@class='cart-qty' and text()='(0)']");
+        if (await emptyCartMessage.isVisible()) {
+            console.log("Cart is already empty.");
+        } else {
+            while (true) {
+                const removeButtons = this.page.locator("//button[@name='updatecart' and @class='remove-btn']");
+                const removeButtonCount = await removeButtons.count();
+                
+                if (removeButtonCount === 0) {
+                    console.log("All items removed from the cart.");
+                    break; 
+                }
+                await removeButtons.nth(0).click();
+                await this.page.waitForTimeout(1000); 
+            }
+        }
+    }
+    async addFirstAndSecondProductsToCart() {
+        const addFirstAndSecondProductsToCart = this.page.locator("//button[contains(text(), 'Add to cart')]");
+        await addFirstAndSecondProductsToCart.nth(0).click();
+        const popupMessageForFirstProduct = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
+        await popupMessageForFirstProduct.waitFor({ state: "visible" });
+            
         await addFirstAndSecondProductsToCart.nth(1).click();
-
-        const popupMessage2 = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
-        await popupMessage2.waitFor({ state: "visible" });
-
+        const popupMessageForSecondProduct = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
+        await popupMessageForSecondProduct.waitFor({ state: "visible" });
+            
         const closeButton = this.page.locator("//span[@class='close' and @title='Close']");
         await closeButton.click();
-
+            
         const cartIcon = this.page.locator("//a[@class='ico-cart']");
         await cartIcon.hover();
         await cartIcon.click();
 
-        
+        // Select the 'terms of service' checkbox
         const termsCheckbox = this.page.locator("//input[@type='checkbox' and @name='termsofservice']");
         await termsCheckbox.check(); // Check the checkbox
+
         const checkoutButton = this.page.locator("//button[@type='submit' and @name='checkout']");
         await checkoutButton.click();
 
@@ -30,3 +54,4 @@ await addFirstAndSecondProductsToCart.nth(0).click();
 
     
 }
+
