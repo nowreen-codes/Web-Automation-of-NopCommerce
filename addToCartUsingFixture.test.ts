57f1ea09d3cc3ae34c14df671926c47f436499c2
Nowreen Islam <nowreen92519@gmail.com>
2025-01-14 09:47:22 +0600

    Update addToCartUsingFixture.test.ts

--- addToCartUsingFixture.test.ts
+++ addToCartUsingFixture.test.ts
@@ -1,19 +1,10 @@
 import { expect, test } from "../base/pomFixture";
-
-/*test.use({
-    browserName: "chromium" // how to run in different browser
-}) */
 const email = "nou@gmail.com";
 const password = "q123#hsjueA";
 
 test.describe("Page object test demo", async () => {
-
-
 test("Register test_01", async ({ page, registrationPage }) => {
-
-   // const register = new RegistrationPage(page);
 await page.goto("https://test460.nop-station.com/en/register?returnUrl=%2Fen%2F");
-
 //expect(register.enterGender()).toBeChecked();
 await registrationPage.enterFirstName("Nowreen");
 await registrationPage.enterLasttName("Islam");
@@ -24,34 +15,23 @@ await registrationPage.enterOptions();
 await registrationPage.enterPassword(password);
 await registrationPage.enterConfirmPassword(password);
 await registrationPage.clickRegister();
-
 })
 
 test("Login test_02", async ({ page, loginPage }) => {
 
-    //const login = new LoginPage(page);
     await page.goto("https://test460.nop-station.com/en/login?returnUrl=%2Fen%2F");
 
     await loginPage.enterEmail(email);
     await loginPage.enterPassword(password);
     await loginPage.checkedRememberMe();
     await loginPage.clickLogin();
-    //expect(await page.title()).toBe("My account");
-
-
-})
+  })
 
 test("Add to cart test_03", async ({ page, loginPage, homePage, books}) => {
-    //const login = new LoginPage(page);
-    //const homepage = new HomePage(page);
-    //const books = new Books(page);
-
     await page.goto("https://test460.nop-station.com/en/login?returnUrl=%2Fen%2F");
     await loginPage.login(email, password);
-   await homePage.clickOnBooks();
-await books.addFirstAndSecondProductsToCart();
-
-
+    await homePage.clickOnBooks();
+    await books.addFirstAndSecondProductsToCart();
 })
 
 test.only("Checkout test_04", async ({ page, loginPage, homePage, books, checkout }) => {
@@ -60,7 +40,6 @@ test.only("Checkout test_04", async ({ page, loginPage, homePage, books, checkou
  await loginPage.login(email, password);
  await homePage.clickOnBooks();
  await books.addFirstAndSecondProductsToCart();    
-
  await checkout.enterFirstName("Nowreen");
  await checkout.enterLastName("Islam");
  await checkout.enterEmail("");
@@ -68,12 +47,11 @@ test.only("Checkout test_04", async ({ page, loginPage, homePage, books, checkou
  await checkout.enterCountryName("Bangladesh");
  await checkout.enterStateName("ঢাকা");
  await checkout.enterCityName("Dhaka");
- await checkout.enterAddress1("Cantonment");
+ await checkout.enterAddress1("Savar");
  await checkout.enterAddress2("");
  await checkout.enterZip("123");
  await checkout.enterPhoneNo("019299999999");
  await checkout.enterFaxNo("");
- 
  await checkout.clickNext();
  await checkout.clickNext2();
  await checkout.clickNext3();  
@@ -81,7 +59,5 @@ test.only("Checkout test_04", async ({ page, loginPage, homePage, books, checkou
  await checkout.clickConfirmOrder();
  await checkout.verifyThankYouMessage();
  await checkout.clickConfirmOrderDetailsLink();
-
 })
-
 })
