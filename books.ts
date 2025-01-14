f648543830a86561aaba5ced6467072778c715bb
Nowreen Islam <nowreen92519@gmail.com>
2025-01-14 09:49:09 +0600

    Update books.ts

--- books.ts
+++ books.ts
@@ -1,25 +1,14 @@
 import { Page } from "@playwright/test";
-
 export default class Books {
-
-    constructor(public page: Page) {
-    }
-
-    async addFirstAndSecondProductsToCart() {
-        // Locate the "Add to cart" buttons
-        const addFirstAndSecondProductsToCart = this.page.locator("//button[contains(text(), 'Add to cart')]");
-
-        // Click the first product's "Add to cart" button
-        await addFirstAndSecondProductsToCart.nth(0).click();
-
-        // Wait for the confirmation of the first product being added to the cart
+constructor(public page: Page) {
+}
+async addFirstAndSecondProductsToCart() {
+const addFirstAndSecondProductsToCart = this.page.locator("//button[contains(text(), 'Add to cart')]");
+await addFirstAndSecondProductsToCart.nth(0).click();
         const popupMessage1 = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
         await popupMessage1.waitFor({ state: "visible" });
-
-        // Click the second product's "Add to cart" button
         await addFirstAndSecondProductsToCart.nth(1).click();
 
-        // Wait for the confirmation of the second product being added to the cart
         const popupMessage2 = this.page.locator("//p[contains(text(), 'The product has been added to your')]");
         await popupMessage2.waitFor({ state: "visible" });
 
@@ -30,13 +19,9 @@ export default class Books {
         await cartIcon.hover();
         await cartIcon.click();
 
-        // Select the 'terms of service' checkbox
+        
         const termsCheckbox = this.page.locator("//input[@type='checkbox' and @name='termsofservice']");
         await termsCheckbox.check(); // Check the checkbox
-
-       // const termandconditionClose = this.page.locator("//button[@type='button' and @title='Close']");
-        // await termandconditionClose.click();
-
         const checkoutButton = this.page.locator("//button[@type='submit' and @name='checkout']");
         await checkoutButton.click();
 
