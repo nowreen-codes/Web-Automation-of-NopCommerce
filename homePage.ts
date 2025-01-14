a210b50726c6e0db982d2a74bd0c1b0e2eb76756
Nowreen Islam <nowreen92519@gmail.com>
2025-01-14 09:38:45 +0600

    Create homePage.ts

---
+++ homePage.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+
+export default class HomePage{
+
+        constructor(public page: Page){
+
+        }
+
+        async clickOnBooks(){
+          await Promise.all([
+            this.page.waitForNavigation({waitUntil:"networkidle"}),
+          
+          this.page.click("//a[contains (text(), 'Books ')] ")
+        ])
+        }
+    }
